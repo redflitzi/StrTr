d018ea819a427e7f727520587b09dd59c22a69c0
redflitzi <35840599+redflitzi@users.noreply.github.com>
2021-12-22 19:16:28 +0100

    Added examples from the PHP  strtr manual

--- Program.cs
+++ Program.cs
@@ -68,14 +68,34 @@ namespace StrtrApp
             Console.WriteLine("input: {0}, output: {1}", input, output);
             Console.WriteLine();
 
-
-
             var animals = "dogcathorsecow";
             var animalsReplace = animals.Replace("dog", "cat").Replace("cat", "horse").Replace("horse", "cow").Replace("cow", "bird");
             var animalsStrTr = animals.StrTr(("dog", "cat"), ("cat", "horse"), ("horse", "cow"), ("cow", "bird"));
 
             Console.WriteLine("animalsReplace: {0}", animalsReplace);
             Console.WriteLine("animalsStrTr: {0}", animalsStrTr);
+            Console.WriteLine();
+
+            Console.WriteLine("\nExamples from the PHP manual:");
+
+            Console.WriteLine("\nExample #1 strtr() example");
+            input = "Bäfoo Dåbar Söderström";
+            Console.WriteLine(input+" => "+input.StrTr("äåö", "aao"));
+
+            Console.WriteLine("\nExample #2 example with two arguments");
+            input = "hi all, I said hello";
+            var trans = new (string, string)[]
+            {
+                ("hello", "hi"),
+                ("hi", "hello")
+            };
+            Console.WriteLine(input+" => "+input.StrTr( trans));
+            
+            Console.WriteLine("\nExample #3 behavior comparison");
+            input = "baab";
+            Console.WriteLine(input+" => "+input.StrTr("ab", "01"));
+            Console.WriteLine(input+" => "+input.StrTr( ("ab", "01") ));
+            Console.WriteLine();
 
         }
     }
